45bad5ea39e7849f91b82dbacdd1541892f45edb
westhousek <nishiiekazuki@gmail.com>
2021-08-19 23:14:31 +0900

    learn bool and string type

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-function add(n1, n2) {
-    if (typeof n1 !== 'number' || typeof n2 !== 'number') {
-        throw new Error('入力値が不正です');
+function add(n1, n2, showResult, phrase) {
+    var result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + result);
     }
-    return n1 + n2;
+    else {
+        return n1 + n2;
+    }
 }
-var number1 = '5';
+var number1 = 5;
 var number2 = 2.4;
-var result = add(number1, number2);
-console.log(result);
+var printResult = true;
+var resulPhrase = 'Result: ';
+add(number1, number2, printResult, resulPhrase);
--- app.ts
+++ app.ts
@@ -1,13 +1,16 @@
-function add(n1: number, n2: number) {
-    // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
-    //     throw new Error('入力値が不正です')
-    // }
-
-    return n1 + n2;
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+    
+    const result = n1 + n2
+    if (showResult) {
+        console.log(phrase + result);
+    } else {
+        return n1 + n2;
+    }
 }
 
-const number1 = '5';
+const number1 = 5;
 const number2 = 2.4;
+const printResult = true;
+const resulPhrase = 'Result: '
 
-const result = add(number1, number2);
-console.log(result);
+add(number1, number2, printResult, resulPhrase);
