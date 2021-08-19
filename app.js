a6212b69dbcf632514aa611bc7c0edee78dd527f
westhousek <nishiiekazuki@gmail.com>
2021-08-19 22:38:53 +0900

    learn number and string type

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('aaaa');
+function add(n1, n2) {
+    if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+        throw new Error('入力値が不正です');
+    }
+    return n1 + n2;
+}
+var number1 = '5';
+var number2 = 2.4;
+var result = add(number1, number2);
+console.log(result);
--- app.ts
+++ app.ts
@@ -1,1 +1,13 @@
-console.log('aaaa')
+function add(n1: number, n2: number) {
+    // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+    //     throw new Error('入力値が不正です')
+    // }
+
+    return n1 + n2;
+}
+
+const number1 = '5';
+const number2 = 2.4;
+
+const result = add(number1, number2);
+console.log(result);
