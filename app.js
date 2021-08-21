5f922f84b3fa2300656e5b247bfdbb76e0fd33da
westhousek <nishiiekazuki@gmail.com>
2021-08-21 10:01:23 +0900

    learn tuple

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var person = {
     name: 'taro',
     age: 30,
-    hobbies: ['sports', 'cooking']
+    hobbies: ['sports', 'cooking'],
+    role: [2, 'author']
 };
+person.role[0] = 10;
+// person.role = [0, 'aaa', 'test']
 var arr = ['a'];
 console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
--- app.ts
+++ app.ts
@@ -1,8 +1,17 @@
-const person = {
+const person: {
+    name: string,
+    age: number,
+    hobbies: string[],
+    role: [number, string] // tuple pushはできる
+} = {
     name: 'taro',
     age: 30,
-    hobbies: ['sports', 'cooking']
+    hobbies: ['sports', 'cooking'],
+    role: [2, 'author'],
 }
+
+person.role[0] = 10;
+// person.role = [0, 'aaa', 'test']
 
 let arr: string[] = ['a'] 
 
