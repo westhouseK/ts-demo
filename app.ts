f403399aa2700035cdc17e333fa573b217a4e3a0
westhousek <nishiiekazuki@gmail.com>
2021-08-21 11:00:10 +0900

    learn Literal

--- app.ts
+++ app.ts
@@ -1,16 +1,22 @@
-function combine(input1: number | string, input2: number | string) {
-
-    let result
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2
-    } else {
-        result = input1.toString() + input2.toString()
-    }
-    return result
+function combine(
+  input1: number | string,
+  input2: number | string,
+  resultConversion: 'as-number' | 'as-text'
+) {
+  let result;
+  if (typeof input1 === "number" && typeof input2 === "number" || resultConversion === 'as-number') {
+    result = +input1 + +input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result
 }
 
-const a = combine(30, 20)
+const a = combine(30, 20, 'as-number')
 console.log(a)
 
-const b = combine('a', 'b')
+const b = combine('a', 'b', 'as-text')
 console.log(b) 
+
+const c = combine('30', '20', 'as-number')
+console.log(c);
