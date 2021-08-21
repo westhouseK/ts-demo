6c09dfd8b3f30d004db21f24382af0d695efda52
westhousek <nishiiekazuki@gmail.com>
2021-08-21 14:09:06 +0900

    learn function type

--- app.js
+++ app.js
@@ -1,29 +1,11 @@
-function add(n1, n2) {
-    return n1 + n2;
+var userInput;
+var userName;
+userInput = 1;
+if (typeof userInput === 'string') {
+    userName = userInput;
 }
-function printResult(num) {
-    console.log('Result: ' + num);
+function generateError(mes, code) {
+    throw { mes: mes, code: code };
 }
-function addAndHandle(n1, n2, cb) {
-    var result = n1 + n2;
-    return cb(result);
-}
-var x = addAndHandle(1, 2, function (result) {
-    console.log(result);
-    return 'ok';
-});
-console.log(x);
-var a;
-a = add;
-// a = printResult
-console.log(a(8, 8));
-printResult(add(1, 2));
-// 注意！これはできるらしい
-function sendRequest(data, cb) {
-    // ... sending a request with "data"
-    return cb({ data: 'Hi there!' });
-}
-sendRequest('Send this!', function (response) {
-    console.log(response);
-    return true;
-});
+var res = generateError('エラー発生', 500);
+console.log(res);
--- app.ts
+++ app.ts
@@ -6,3 +6,10 @@ userInput = 1
 if (typeof userInput === 'string') {
   userName = userInput
 }
+
+function generateError(mes: string, code: number): never {
+  throw { mes, code }
+}
+
+const res = generateError('エラー発生', 500)
+console.log(res); 
