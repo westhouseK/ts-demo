e533b84bcd98c776868e72d5ba4863c986f40b78
westhousek <nishiiekazuki@gmail.com>
2021-08-21 11:03:03 +0900

    learn alias

--- app.ts
+++ app.ts
@@ -1,7 +1,10 @@
+type Conbinable = number | string
+type ConversionDescriptor = 'as-number' | 'as-text'
+
 function combine(
-  input1: number | string,
-  input2: number | string,
-  resultConversion: 'as-number' | 'as-text'
+  input1: Conbinable | string,
+  input2: Conbinable | string,
+  resultConversion: ConversionDescriptor
 ) {
   let result;
   if (typeof input1 === "number" && typeof input2 === "number" || resultConversion === 'as-number') {
