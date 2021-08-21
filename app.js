d5a4ecd7acbeedf42a70aaadcc0d1f8afa6b5f48
westhousek <nishiiekazuki@gmail.com>
2021-08-21 11:28:10 +0900

    learn function type

--- app.js
+++ app.js
@@ -4,4 +4,8 @@ function add(n1, n2) {
 function printResult(num) {
     console.log('Result: ' + num);
 }
+var a;
+a = add;
+// a = printResult
+console.log(a(8, 8));
 printResult(add(1, 2));
--- app.ts
+++ app.ts
@@ -6,4 +6,8 @@ function printResult(num: number): void {
   console.log('Result: ' + num)
 }
 
+let a: (a: number, b: number) => number;
+a = add
+// a = printResult
+console.log(a(8, 8))
 printResult(add(1, 2))
