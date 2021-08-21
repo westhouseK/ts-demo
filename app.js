32fea53fa92affdc9d5e05e1d7c40707f79be924
westhousek <nishiiekazuki@gmail.com>
2021-08-21 09:55:26 +0900

    learn array type

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 var person = {
     name: 'taro',
-    age: 30
+    age: 30,
+    hobbies: ['sports', 'cooking']
 };
+var arr = ['a'];
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toLocaleUpperCase());
+}
--- app.ts
+++ app.ts
@@ -1,6 +1,13 @@
 const person = {
     name: 'taro',
-    age: 30
+    age: 30,
+    hobbies: ['sports', 'cooking']
 }
 
+let arr: string[] = ['a'] 
+
 console.log(person.name)
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toLocaleUpperCase())
+}
