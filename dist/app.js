1a6296e60358ec88a0d88ec00d9d129b251e19a0
westhousek <nishiiekazuki@gmail.com>
2021-08-22 22:02:14 +0900

    learn class

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,14 @@
 "use strict";
-let age = 30;
+class Department {
+    constructor(n) {
+        this.name = n;
+    }
+    discribe() {
+        console.log('Department: ' + this.name);
+    }
+}
+const accouting = new Department('Accounting');
+accouting.discribe();
+const accountingCoby = { name: 'dummy', discribe: accouting.discribe };
+accountingCoby.discribe();
 //# sourceMappingURL=app.js.map
