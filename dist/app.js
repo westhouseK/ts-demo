ab7f24a205d10d0cc5c7c17762ba6bf5f7cae074
westhousek <nishiiekazuki@gmail.com>
2021-08-25 22:36:59 +0900

    learn discrimiated unions

--- dist/app.js
+++ dist/app.js
@@ -40,4 +40,17 @@ function useVehicle(vehicle) {
 }
 useVehicle(v1);
 useVehicle(v2);
+function moveAnimal(animal) {
+    let speed;
+    switch (animal.type) {
+        case 'bird':
+            speed = animal.flyingSpeed;
+            break;
+        case 'horse':
+            speed = animal.runningSpeed;
+            break;
+    }
+    console.log(speed);
+}
+moveAnimal({ type: 'horse', runningSpeed: 100 });
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -71,3 +71,29 @@ function useVehicle(vehicle: Vehicle) {
 
 useVehicle(v1)
 useVehicle(v2)
+
+interface Bird {
+  type: 'bird',
+  flyingSpeed: number
+}
+
+interface Horse {
+  type: 'horse',
+  runningSpeed: number
+}
+
+type Animal = Bird | Horse
+
+function moveAnimal(animal: Animal) {
+  let speed;
+  switch(animal.type) {
+    case 'bird':
+      speed = animal.flyingSpeed
+      break
+    case 'horse':
+      speed = animal.runningSpeed
+      break
+  }
+  console.log(speed)
+}
+moveAnimal({type: 'horse', runningSpeed: 100})
