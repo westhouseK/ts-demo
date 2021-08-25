c2eb42b1442c3de821c386ce2d9fffde01530fc2
westhousek <nishiiekazuki@gmail.com>
2021-08-25 22:21:55 +0900

    learn type guard

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,43 @@
 "use strict";
-const el = {
+const e1 = {
     name: 'Max',
     privileges: ['create-server'],
     startDate: new Date()
 };
+function add(a, b) {
+    if (typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+function printEmployeeInfo(emp) {
+    console.log(emp.name);
+    if ('privileges' in emp) {
+        console.log(emp.privileges);
+    }
+}
+printEmployeeInfo({ name: 'a', startDate: new Date() });
+class Car {
+    drive() {
+        console.log('運転中');
+    }
+}
+class Truck {
+    drive() {
+        console.log('トラック運転中');
+    }
+    loadCargo(acount) {
+        console.log(acount);
+    }
+}
+const v1 = new Car();
+const v2 = new Truck();
+function useVehicle(vehicle) {
+    vehicle.drive();
+    if ('loadCargo' in vehicle) {
+        vehicle.loadCargo(1);
+    }
+}
+useVehicle(v1);
+useVehicle(v2);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,3 @@
-
-
 // 交差型
 type Admin = {
   name: string,
@@ -14,7 +12,7 @@ type Employee = {
 // interface EleccatedEmploee extends Admin, Employee {}
 type EleccatedEmploee = Admin & Employee
 
-const el: EleccatedEmploee = {
+const e1: EleccatedEmploee = {
   name: 'Max',
   privileges: ['create-server'],
   startDate: new Date()
@@ -24,3 +22,52 @@ type Combinable = string | number
 type Numeric = number | boolean
 
 type Universal = Combinable & Numeric
+
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === 'string' || typeof b === 'string') {
+    return a.toString() + b.toString()
+  } 
+  return a + b;
+}
+
+type UnknownEployee = Employee | Admin
+
+function printEmployeeInfo(emp: UnknownEployee) {
+  console.log(emp.name)
+  if ('privileges' in emp) {
+    console.log(emp.privileges)
+  }
+}
+
+printEmployeeInfo({name: 'a', startDate: new Date()})
+
+class Car {
+  drive() {
+    console.log('運転中')
+  }
+}
+
+class Truck {
+  drive() {
+    console.log('トラック運転中')
+  }
+
+  loadCargo(acount: number) {
+    console.log(acount)
+  }
+}
+
+type Vehicle = Car | Truck
+
+const v1 = new Car()
+const v2 = new Truck()
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive()
+  if ('loadCargo' in vehicle) {
+    vehicle.loadCargo(1);
+  } 
+}
+
+useVehicle(v1)
+useVehicle(v2)
