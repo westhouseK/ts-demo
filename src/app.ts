b4330d032447724d52eebd114361fb7469df678c
westhousek <nishiiekazuki@gmail.com>
2021-09-14 22:32:26 +0900

    learn declear

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,6 @@
-import { ProjectInput } from "./components/project-input";
-import { ProjectList } from "./components/project-list";
+import _ from "lodash";
 
-new ProjectInput();
-new ProjectList("active");
-new ProjectList("finished");
+console.log(_.shuffle([1, 2, 3]));
 
-console.log('aa')
+declare var GLOBAL: any;
+console.log(GLOBAL);
