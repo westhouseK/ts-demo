d6857d3ea33e40cc5698ca58af068953c8e943ac
westhousek <nishiiekazuki@gmail.com>
2021-11-13 04:11:30 +0900

    commit

--- build/js/test.js
+++ build/js/test.js
@@ -1,4 +1,10 @@
 "use strict";
-var a = '1';
-console.log(a);
+function* g(n) {
+    while (n < 100) {
+        yield n;
+        n++;
+    }
+}
+for (const n of g(10))
+    console.log(n);
 //# sourceMappingURL=test.js.map
--- src/test.ts
+++ src/test.ts
@@ -1,2 +1,8 @@
-const a = '1'
-console.log(a)
+function* g<T extends number>(n :T) {
+  while (n < 10) {
+      yield n;
+      n++
+  }
+}
+for (const n of g(1)) console.log(n)
+console.log(1)
