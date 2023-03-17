3bfa77589dd799f2640b54fda507c97863789551
westhouseK <nishiiekazuki@gmail.com>
2023-03-17 23:29:14 +0900

    almost complate

--- src/ddd.ts
+++ src/ddd.ts
@@ -1,51 +1,87 @@
-// 参照→テイクアウトの時、軽減税率 
-// 登録→商品の名前は10文字以内
+// 仕様
+// 商品名 10文字以内
+// 値段 100〜10000円
+// ステータス 一度完了にしたら、変更することができない
+
+type Status = 'waiting' | 'complated'
 
 // ドメイン層
 class Item {
     private _id: number
     private _name: string
     private _price: number
-    private isTakeout: boolean
-    constructor(name: string, price: number, isTakeout: boolean) {
+    private _status: Status
+    constructor(name: string, price: number) {
         this._id = Math.random()
         if (name.length >= 10) {
-            throw new Error('名前は10文字以内です')
+            throw new Error('商品名は10文字以内です')
+        }
+        if (price < 100 || price > 10000) {
+            throw new Error('値段は100〜10000円です')
         }
         this._name = name
         this._price = price
-        this.isTakeout = isTakeout
+        this._status = 'waiting'
     }
-
-    public get name() : string {
+    public name() {
         return this._name
     }
-
-    getTaxPrice = () => {
-        return this.isTakeout ? this._price * 1.08 : this._price * 1.1
+    public status() {
+        return this._status
+    }
+    complate() {
+        this._status = 'complated'
+    }
+}
+class ItemDomainService {
+    notChangeStatus(item: Item) {
+        if (item.status() === 'complated') {
+            throw new Error('一度完了にしたら、変更することができません')
+        }
     }
 }
 interface IItemRepository {
-    findById(id: number) :void
-    save(item: Item): void
+    create(item: Item): void
+    update(item: Item): void
 }
+
 
 // アプリケーション(ユースケース)層
 class CreateItemUsecase {
-    constructor(private repo: IItemRepository) {}
-    execute = (name: string, price: number, isTakeout: boolean) => {
-        const item = new Item(name, price, isTakeout)
-        this.repo.save(item)
+    constructor(private repo: IItemRepository) { }
+    execute(name: string, price: number) {
+        const item = new Item(name, price)
+        this.repo.update(item)
+    }
+}
+class UpdateItemUsecase {
+    constructor(private repo: IItemRepository, private itemDomainService: ItemDomainService) { }
+    execute(id: number, name: string, price: number) {
+        // FIXME: 本当はデータベースから参照
+        const item = new Item(name, price)
+        this.itemDomainService.notChangeStatus(item)
+        this.repo.create(item)
     }
 }
 
 // プレゼンテーション層
 class CreateItemResolver {
-    constructor(private createItemUsecase: CreateItemUsecase) {}
-    createItem(name: string, price: number, isTakeout: boolean) {
+    constructor(private createItemUsecase: CreateItemUsecase) { }
+    createItem(name: string, price: number) {
         // TODO: 本当はInputを定義
 
-        const usecase = this.createItemUsecase.execute(name, price, isTakeout)
+        const usecase = this.createItemUsecase.execute(name, price)
+
+        // TODO: 本当はOutputを定義
+
+    }
+}
+class UpdateItemResolver {
+    constructor(private updateItemUsecase: UpdateItemUsecase) { }
+    updateItem(id: number, name: string, price: number) {
+        // TODO: 本当はInputを定義
+
+        const usecase = this.updateItemUsecase.execute(id, name, price)
 
         // TODO: 本当はOutputを定義
 
@@ -53,24 +89,36 @@ class CreateItemResolver {
 }
 
 // インフラ層
-class ItemRepository implements IItemRepository{
-    findById(id: number): void {
-        console.log(`データベースから${id}で検索`)
+class ItemRepository implements IItemRepository {
+    create(item: Item) {
+        console.log(`データベースに${item.name()}を登録`)
     }
-    save(item: Item): void {
-        console.log(`データベースに${item.name}を保存`)
+    update(item: Item) {
+        console.log(`データベースに${item.name()}を更新`)
     }
 }
 
-const resolver = new CreateItemResolver(new CreateItemUsecase(new ItemRepository))
+// 新規作成
+const createItemResolver = new CreateItemResolver(new CreateItemUsecase(new ItemRepository))
 // 正常系
-resolver.createItem('coffee1', 500, true)
+createItemResolver.createItem('coffee1', 500)
 // 異常系
 try {
-    resolver.createItem('coffeecoffee123', 500, false)
+    createItemResolver.createItem('coffeecoffee123', 500)
 } catch (e: any) {
     console.log(`Error: ${e.message}`)
 }
+try {
+    createItemResolver.createItem('coffee100', 100000)
+} catch (e: any) {
+    console.log(`Error: ${e.message}`)
+}
+
+// 更新
+const updateItemResolver = new UpdateItemResolver(new UpdateItemUsecase(new ItemRepository, new ItemDomainService()))
+// 正常系
+updateItemResolver.updateItem(1, 'coffee123', 1000)
+
 
 
 
